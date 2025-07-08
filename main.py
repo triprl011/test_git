eed6e813b89864ae1f321a0094ec169dc2419949
Vitalii Grebnev <v.grebnev@mwnts.ru>
2025-07-08 22:54:14 +0300

    we added new function

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ def print_hi1(name):
     print(f'Hi from your collegue, {name}')  # Press ⌘F8 to toggle the breakpoint.
 # we change code remotly and now we want to get it from remore poject(changes are made our teammate)
 
+def print_hi2(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi 2 from your collegue 2, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# we change code remotly and now we want to get it from remore poject(changes are made our teammate)
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi2('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
