3b515a17d279f34c04f20b69fed22ce94058d971
Vitalii Grebnev <v.grebnev@mwnts.ru>
2025-07-16 17:39:38 +0300

    refactoring

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def print_hi3(name):
     print(f'Hi from your collegue, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
+
+def print_hi4(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi from your collegue, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
 # we change code remotely and now we want to get it from remore poject(changes are made our teammate)
 
 def print_hi2(name):
